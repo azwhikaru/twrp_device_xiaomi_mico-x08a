ebabf38502e364ea0a6adb43e199f1e38b82d44d
Aizawa Hikaru <37921907+azwhikaru@users.noreply.github.com>
2022-02-08 23:02:51 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,8 +93,8 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 # To minimize the image volume ↓
 # TW_USE_TOOLBOX := true
 # TW_EXTRA_LANGUAGES := false
-TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0 libcryptfs_hw
-TW_RECOVERY_ADDITIONAL_RELINK_FILES := android.hardware.boot@1.0.so
+TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
+# TW_RECOVERY_ADDITIONAL_RELINK_FILES := android.hardware.boot@1.0.so
 
 # Excludes
 TW_EXCLUDE_DEFAULT_USB_INIT := true
